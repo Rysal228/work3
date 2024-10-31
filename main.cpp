159c358aa01bbca1860ea55c7272bcfd4605217c
Sergey Cheradionov <serg.cheradionov@mail.ru>
2024-10-31 23:50:29 +0700

    fast

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 double my_pow(double number, int degree) {
-	answer = number;
-	for (int i = 0; i < degree; i++)
-	{
-		answer = answer * number;
-	}
+    double answer = 1.0;
+    while (degree > 0) {
+        if (degree % 2 == 1) {               
+            answer = answer * number;
+        }
+        number = number* number;
+        degree = degree/2;
+    }
 	return answer;
 }
