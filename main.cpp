4d791ea540cea8c9606556425ffcb90bd0f25771
Sergey Cheradionov <serg.cheradionov@mail.ru>
2024-10-31 23:57:52 +0700

    correctworkwithnegativenumbers

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 double my_pow(double number, int degree) {
-	answer = number;
+	if (degree < 0) {
+		bool isNegativeDegree = 1;
+	}
+	if (isNegativeDegree) {
+		number = 1 / number; 
+		degree = -degree;      
+	}
+	double answer = number;
 	for (int i = 0; i < degree; i++)
 	{
 		answer = answer * number;
